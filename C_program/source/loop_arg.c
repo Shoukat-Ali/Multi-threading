124a746ddf7f22527bde2bf52c628c1c2a4982de
Shoukat Ali <shoukat.1983@gmail.com>
2024-11-04 14:15:06 -0700

    pthread_self() is added to thread creation and argument passing in loop program

--- C_program/source/loop_arg.c
+++ C_program/source/loop_arg.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>     // Required for syscall()
 #include <sys/syscall.h>  // Required for SYS_* constant
+#include <pthread.h>
 #include "../header/loop_arg.h"
 
 
@@ -25,6 +26,34 @@ void* display_prime(void *PNum)
             pnum = 1;
         }
     }
-    printf("Thread id: %ld, prime number: %d\n", syscall(SYS_gettid), pnum);
+    /**
+     * long syscall(long number, ...);
+     * 
+     * syscall() - indirect system call
+     * syscall() is a small library function that invokes the system call 
+     * whose assembly language interface has the specified number with the 
+     * specified arguments. Employing syscall() is useful, for example, 
+     * when invoking a system call that has no wrapper function in the C library.
+     * 
+     * Symbolic constants for system call numbers can be found in the header file <sys/syscall.h>.
+     * 
+     * The return value is defined by the system call being invoked. In general, 
+     * a 0 return value indicates success.  A -1 return value indicates an error, 
+     * and an error number is stored in errno.
+     * 
+     * Note: syscall() is system-dependent, therefore, portability issue across different platforms 
+     */
+    printf("Thread id using syscall()     : %ld, \t\t prime number: %d\n", syscall(SYS_gettid), pnum);
+
+    /**
+     * pthread_t pthread_self(void);
+     * 
+     * The pthread_self() function returns the ID of the calling thread. 
+     * This is the same value that is returned in *thread in the pthread_create(3) 
+     * call that created this thread.
+     * 
+     * This function always succeeds, returning the calling thread's ID.
+     */
+    printf("Thread id using pthread_self(): %lu, prime number: %d\n", (unsigned long)pthread_self(), pnum);
     return NULL;
 }
