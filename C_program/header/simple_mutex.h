9403c7c3c1964cf37d030bebe201ab118f484822
Shoukat Ali <shoukat.1983@gmail.com>
2024-11-05 17:16:35 -0700

    main function logic updated and error for mutex init and destory added to simple mutex program

--- C_program/header/simple_mutex.h
+++ C_program/header/simple_mutex.h
@@ -9,6 +9,8 @@
 
 #define ERR_THRD_CREATE 1
 #define ERR_THRD_JOIN 2
+#define ERR_MUTEX_INIT 3
+#define ERR_MUTEX_DESTORY 4
 
 extern int shared;
 extern pthread_mutex_t lock;
--- C_program/test/test_simple_mutex.c
+++ C_program/test/test_simple_mutex.c
@@ -5,9 +5,7 @@
 */
 
 #include <stdio.h>
-#include <unistd.h>
 #include <pthread.h>
-#include <sys/syscall.h>
 #include "../header/simple_mutex.h"
 
 #define NUM_THREADS 7
@@ -36,40 +34,47 @@ int main()
      * otherwise, an error number shall be returned to indicate the error.
      * 
      */
-    if(!pthread_mutex_init(&lock, NULL)) {   
-        // Successful mutex initialization
-
-        for(i = 0; i < NUM_THREADS; ++i) {
-            // starting a new thread within the process
-            if(pthread_create(thrd + i, NULL, &simple_thread, NULL)) {
-                printf("Thread no. %d creation failed\n", i + 1);
-                return ERR_THRD_CREATE;
-            }
-            //printf("Thread no. %d has started\n", i + 1);
-        }
-        
-        for(i = 0; i < NUM_THREADS; ++i) {
-            // Wait for the thread to end execution
-            if(pthread_join(thrd[i] , NULL)){
-                printf("Thread no. %d failed to terminate\n", i + 1);
-                return ERR_THRD_JOIN;
-            }
-            //printf("Thread no. %d has ended\n", i + 1);
-        }
-        /**
-         * int pthread_mutex_destroy(pthread_mutex_t *mutex);
-         * 
-         * The pthread_mutex_destroy() function shall destroy the mutex object 
-         * referenced by mutex; the mutex object becomes, in effect, uninitialized. 
-         * An implementation may cause pthread_mutex_destroy() to set the object 
-         * referenced by mutex to an invalid value.
-         * 
-         */
-        if(pthread_mutex_destroy(&lock)) {
-            // Destroying mutex caused an error
-            perror("Failed to destroy mutex");
-        }
-        printf("Incrementing the same int variable by all threads: %d\n", shared);
+    if(pthread_mutex_init(&lock, NULL)) {
+        // Mutex initialization failed
+        perror("Failed to initialize the mutex");
+        return ERR_MUTEX_INIT;
     }
+
+    // Successful mutex initialization
+    for(i = 0; i < NUM_THREADS; ++i) {
+        // starting a new thread within the process
+        if(pthread_create(thrd + i, NULL, &simple_thread, NULL)) {
+            printf("Thread no. %d creation failed\n", i + 1);
+            return ERR_THRD_CREATE;
+        }
+        //printf("Thread no. %d has started\n", i + 1);
+    }
+    
+    for(i = 0; i < NUM_THREADS; ++i) {
+        // Wait for the thread to end execution
+        if(pthread_join(thrd[i] , NULL)){
+            printf("Thread no. %d failed to terminate\n", i + 1);
+            return ERR_THRD_JOIN;
+        }
+        //printf("Thread no. %d has ended\n", i + 1);
+    }
+    /**
+     * int pthread_mutex_destroy(pthread_mutex_t *mutex);
+     * 
+     * The pthread_mutex_destroy() function shall destroy the mutex object 
+     * referenced by mutex; the mutex object becomes, in effect, uninitialized. 
+     * An implementation may cause pthread_mutex_destroy() to set the object 
+     * referenced by mutex to an invalid value.
+     * 
+     * If successful, the pthread_mutex_destroy() functions shall return zero; 
+     * otherwise, an error number shall be returned to indicate the error.
+     */
+    if(pthread_mutex_destroy(&lock)) {
+        // Destroying mutex caused an error
+        perror("Failed to destroy mutex");
+        return ERR_MUTEX_DESTORY;
+    }
+    printf("Incrementing the same int variable by all threads: %d\n", shared);
+
     return 0;
 }
