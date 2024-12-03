148dfe2dde3a1e2ad74d79e2115a4ecef0383025
Shoukat Ali <shoukat.1983@gmail.com>
2024-12-03 13:10:39 -0700

    Simple semaphore program is updated before testing

--- C_program/header/simple_semaphore.h
+++ C_program/header/simple_semaphore.h
@@ -5,14 +5,45 @@
 #ifndef SEMAPHORE_H
 #define SEMAPHORE_H
 
+#include <stdio.h>
 #include <semaphore.h>  // For semaphore
 
+/**
+ * Based on the POSIX manual, The <semaphore.h> header shall define the sem_t type, 
+ * used in performing semaphore operations. The semaphore may be implemented
+ * using a file descriptor, in which case applications are able to open up 
+ * at least a total of {OPEN_MAX} files and semaphores.
+ * 
+ * The <semaphore.h> header shall define the symbolic constant SEM_FAILED 
+ * which shall have type sem_t *.
+ * 
+ */
 extern sem_t semUser; // Declaration
+
+
+// TODO: bound check on the number of threads
+#define NUM_THREAD 17
+#define SEMAPHORE_LIMIT 7
+
+#define ERR_SEMAPHORE_INIT 1
+#define ERR_THRD_CREATE 2
+#define ERR_THRD_JOIN 3
+
+
+int* my_queue[NUM_THREAD] = {NULL};  // Array of pointers
 
 
 /**
  * 
 */
 void* my_thrd();
+
+
+/**
+ * The functions attempts to clean up the utilized resources.
+ * If the clean up was not successful, then the error is reported
+ * 
+ */
+void clean_up();
 
 #endif
--- C_program/source/simple_semaphore.c
+++ C_program/source/simple_semaphore.c
@@ -6,6 +6,7 @@
 #include "../header/simple_semaphore.h"
 
 
+
 /**
  * 
 */
@@ -26,11 +27,76 @@ void* my_thrd(void* indx)
         return NULL;
     }
      
-    printf("(%d) Waiting in the login queue\n", index);
-    sem_wait(&semUser);
-    printf("(%d) Logged in\n", index);
-    sleep(2);
-    printf("(%d) Logged out\n", index);
-    sem_post(&semUser);
+    printf("User: (%d) is waiting in the login queue\n", index);
+    /**
+     * int sem_wait(sem_t *sem);
+     * 
+     * sem_wait() decrements (locks) the semaphore pointed to by sem.
+     * If the semaphore's value is greater than zero, then the decrement proceeds, 
+     * and the function returns, immediately. If the semaphore currently has the 
+     * value zero, then the call blocks until either it becomes possible to perform 
+     * the decrement (i.e., the semaphore value rises above zero), 
+     * or a signal handler interrupts the call.
+     * 
+     * The function returns 0 on success; on error, the value of the semaphore 
+     * is left unchanged, -1 is returned, and errno is set to indicate the error.
+     * 
+     */
+    if(sem_wait(&semUser)) {
+        perror("Error, sem_wait() failed");
+    }
+    printf("User: (%d) is logged in\n", index);
+    // Sleeping for nearly a minute
+    if(usleep(999999)) {
+        perror("Error, usleep() failed");
+    }
+    printf("User: (%d) is logged out\n", index);
+    /**
+     * int sem_post(sem_t *sem);
+     * 
+     * sem_post() increments (unlocks) the semaphore pointed to by sem.
+     * If the semaphore's value consequently becomes greater than zero, 
+     * then another process or thread blocked in a sem_wait() call will
+     * be woken up and proceed to lock the semaphore.
+     * 
+     * On success, returns 0; on error, the value of the semaphore is left 
+     * unchanged, -1 is returned, and errno is set to indicate the error.
+     * 
+     */
+    if (sem_post(&semUser)) {
+        perror("Error, sem_post() failed");
+    }
     return NULL;
+}
+
+
+
+/**
+ * The functions attempts to clean up the utilized resources.
+ * If the clean up was not successful, then the error is reported
+ * 
+ */
+void clean_up()
+{
+    for(int i = 0; i < NUM_THREAD; i++) {
+        free(my_queue[i]);
+        my_queue[i] = NULL;
+    }
+
+    /**
+     * int sem_destroy(sem_t *sem);
+     * 
+     * sem_destroy() destroys the unnamed semaphore at the address pointed to by sem. 
+     * 
+     * Only a semaphore that has been initialized by sem_init(3) should be destroyed 
+     * using sem_destroy(). Destroying a semaphore that other processes or threads are
+     * currently blocked on (in sem_wait(3)) produces undefined behavior.
+     * 
+     * On success, returns 0; on error, -1 is returned, and errno is set to indicate the error.
+     * 
+     */
+    if(sem_destroy(&semUser)) {
+        perror("Error, destroying semaphore failed");
+    }
+
 }
