404a6d4efaff46dfc5bbe090a648451b91c56b61
Shoukat Ali <shoukat.1983@gmail.com>
2024-12-31 10:10:21 -0700

    Description added in comments and some minor changes

--- CPP_program/header/simple_mutex.hpp
+++ CPP_program/header/simple_mutex.hpp
@@ -14,9 +14,24 @@
 class SimpleMutex {
     private:
         const int NumThreads;
-        const int Count;
+        const int count;
         int SharedRsc;
-        mutable std::mutex mtx;
+        /**
+         * mutex offers exclusive, non-recursive ownership semantics:
+         *      A calling thread owns a mutex from the time that it successfully calls 
+         *      either lock or try_lock until it calls unlock.
+         *      When a thread owns a mutex, all other threads will block (for calls to lock) 
+         *      or receive a false return value (for try_lock) if they attempt to claim ownership 
+         *      of the mutex.
+         *      A calling thread must not own the mutex prior to calling lock or try_lock.
+         * 
+         * The behavior of a program is undefined if a mutex is destroyed while still owned by any threads, 
+         * or a thread terminates while owning a mutex. The mutex class satisfies all requirements of 
+         * Mutex and StandardLayoutType.
+         * 
+         * std::mutex is neither copyable nor movable. 
+         */
+        std::mutex mtx;
 
     public:
         SimpleMutex(int nthrds, int cnt);       //Constructor
--- CPP_program/source/simple_mutex.cpp
+++ CPP_program/source/simple_mutex.cpp
@@ -8,7 +8,7 @@
 /**
  * 
  */
-SimpleMutex::SimpleMutex(int nthrds, int cnt) : NumThreads(nthrds), Count(cnt), SharedRsc(0) 
+SimpleMutex::SimpleMutex(int nthrds, int cnt) : NumThreads(nthrds), count(cnt), SharedRsc(0) 
 {
     if (nthrds <= 0 || cnt <= 0) {
         // For now, simply printing error message
@@ -16,6 +16,42 @@ SimpleMutex::SimpleMutex(int nthrds, int cnt) : NumThreads(nthrds), Count(cnt), 
         std::cout << "Number of threads and count value must be positive\n";
     }
 }
+
+
+/**
+ * 
+ */
+void SimpleMutex::increment_count() 
+{
+    std::cout << "Thread id:" << std::this_thread::get_id() << "\n";
+    for (int i = 0; i < count; ++i) {
+        /**
+         * void lock();
+         * 
+         * Locks the mutex. If another thread has already locked the mutex, 
+         * a call to lock will block execution until the lock is acquired.
+         * 
+         * If lock is called by a thread that already owns the mutex, the behavior is undefined: 
+         * for example, the program may deadlock. An implementation that can detect the invalid 
+         * usage is encouraged to throw a std::system_error with error condition 
+         * resource_deadlock_would_occur instead of deadlocking. 
+         */
+        mtx.lock();
+        
+        // Criticial resource/section or atomic code
+        SharedRsc++;
+
+        /**
+         * void unlock();
+         * 
+         * Unlocks the mutex. The mutex must be locked by the current thread of execution, 
+         * otherwise, the behavior is undefined.
+         *   
+         * */  
+        mtx.unlock();
+    }
+}
+
 
 
 /**
@@ -51,32 +87,13 @@ void SimpleMutex::run()
 int SimpleMutex::get_count() 
 {
     int CurrVal = -1;
+    // Lock mutex
     mtx.lock();
     CurrVal = SharedRsc;
+    // Unlock mutex
     mtx.unlock();
 
     return CurrVal;
 }
 
 
-/**
- * 
- */
-void SimpleMutex::increment_count() 
-{
-    std::cout << "Thread id:" << std::this_thread::get_id() << "\n";
-    for (int i = 0; i < Count; ++i) {
-        // Locks the mutex. If another thread has already locked the mutex, 
-        // a call to lock will block execution until the lock is acquired.
-        // syntax: void lock();
-        mtx.lock();
-        
-        // Criticial section or atomic code
-        SharedRsc++;
-
-        // unlocks the mutex
-        // The mutex must be locked by the current thread of execution, otherwise, the behavior is undefined.
-        // syntax: void unlock();  
-        mtx.unlock();
-    }
-}
